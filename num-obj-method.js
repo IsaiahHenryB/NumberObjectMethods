678bba6ca8ac95a51bdf1e651f73db26cd6effce
IsaiahHenry <isaiahhenryb@gmail.com>
2021-03-13 23:36:41 -0500

    Complete

---
+++ num-obj-method.js
@@ -0,0 +1,68 @@
+//place answer below each challenge. Be sure to test your results in the  colsole.
+
+//Hint, you can find examples on the slides!
+
+//Example: Convert the integer 321 into a string
+//Example answer: 
+let x  = 321;
+let y = x.toString();
+console.log(y)
+//Example result:“321”
+
+// -------------------------------------------------------------
+//1: Convert 10 into a string
+//1 Answer:
+let a  = 10;
+let b = a.toString();
+console.log(b);
+//1 Result:
+
+// 10
+//2: Convert 1400 into a string
+//2 Answer:
+let c  = 1400;
+let d = c.toString();
+console.log(d);
+//2 Result:
+
+// 1400
+//3: Convert 103456 into a string
+//3 Answer:
+let e  = 103456;
+let f = e.toString();
+console.log(f);
+//3 Result:
+// 103456
+//4: Convert the boolean true and false into integers
+//4 Answer:
+let cool = true;
+console.log(Number(cool));
+let notCool = false;
+console.log(Number(notCool));
+//4 Result:
+// 1,0
+
+//4: Convert the boolean true and false into integers
+//4 Answer:
+
+//4 Result:
+
+//5: Convert new Date into integers
+//5 Answer:
+let date = new Date();
+console.log(Number(date));
+//5 Result:
+// IT KEEPS CHANGING!!! D:
+//6: Convert the string "02130" into an integer
+//6 Answer:
+let fakeInteger = "02130";
+console.log(Number(fakeInteger));
+//6 Result:
+// 2310
+//7: Use "03 1010" string as a value in a variable in an attempt to convert into an integer. Be sure to include the quotations.
+//7 Answer:
+let wack = "03 1010"
+console.log(Number(wack))
+//7 Result:
+// NaN
+
